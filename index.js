201eedefdee9c697dfe601e19414011b445e7c41
JJ <jj.vega86@gmail.com>
2022-09-14 07:10:57 -0400

    📦 NEW: Create Node CLI init

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * dcc-notion
+ * Perform common administration tasks for maintaining the devCodeCamp Notion workspace.
+ *
+ * @author JJ Vega <https://github.com/jjvega86>
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+	init({ clear });
+	input.includes(`help`) && cli.showHelp(0);
+
+	debug && log(flags);
+})();
